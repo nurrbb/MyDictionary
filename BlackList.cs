6fb73889cb1fc9105debf9d89949cdc612c776db
Nur B <59769129+nurrbb@users.noreply.github.com>
2021-02-07 22:06:10 +0300

    Dictionary Examp

---
+++ BlackList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class BlackList <TCode,TName>
+    {
+        TCode[] Codes;
+        TName[] names;
+
+        public BlackList()
+        {
+            Codes = new TCode[0];
+            names = new TName[0];
+        }
+        public void Add(TCode code, TName name)
+        {
+            TCode[] tempCode = Codes;
+            TName[] tempnames = names;
+
+            Codes = new TCode[Codes.Length + 1];
+            names = new TName[names.Length + 1];
+            for (int i = 0; i < tempCode.Length; i++) 
+            {
+                Codes[i] = tempCode[i];
+            }
+
+            Codes[Codes.Length - 1] = code;
+
+            for (int i = 0; i < tempnames.Length; i++)
+            {
+                names[i] = tempnames[i];
+            }
+
+            names[names.Length - 1] = name;
+
+            Console.WriteLine("Blacklist Code : {0} --- Name/Surname : {1}", code, name);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> BlackList = new Dictionary<int, string>();
+
+            BlackList.Add(1, "Elizabeth Keen"); 
+            BlackList.Add(3, "Katarina Rostova");
+            BlackList.Add(4, "Kathryn Nemec");
+            BlackList.Add(7, "Tom Keen");
+            BlackList.Add(10, "Dembe Zuma");
+            BlackList.Add(12, "Alan Fitch");
+            BlackList.Add(13, "Raymond Reddington");
+
+            foreach (var number in BlackList)
+            {
+                Console.WriteLine(number);
+            }
+
+
+
+
+
+
+        }
+    }
+}
